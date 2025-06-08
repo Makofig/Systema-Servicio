2b5f685f9d4a634b0e4f0baab828348b237c42b4
Marcos <Markof1089@outlook.com.ar>
2025-06-08 18:03:32 -0300

    se corrigio ver los pagos en la vista de deudores

--- src/list/getClientesByCuota.php
+++ src/list/getClientesByCuota.php
@@ -16,19 +16,21 @@ if(isset($_GET['cuota'])) {
     // Aquí deberías realizar la consulta a la base de datos para obtener los clientes de la cuota seleccionada
     // Por ejemplo, asumiendo que la tabla de clientes se llama 'clientes', y el campo que almacena la cuota se llama 'num_cuotas'
     // Debes reemplazar esto con tu consulta real
-    $consulta = $db->prepare("SELECT cl.*, p.*, co.* FROM cliente cl "
+    $consulta = $db->prepare("SELECT cl.*, p.*, co.*, p.id as id_pagos FROM cliente cl "
             . "INNER JOIN pagos p ON p.id_cliente = cl.id "
             . "INNER JOIN cuotas co ON p.id_cuota = co.id "
             . "WHERE p.num_cuotas = ? and YEAR(co.fecha_emision) = ? and p.estado = '0';");
     $consulta->bind_param("ss", $cuota, $year);
     $consulta->execute();
     $resultado = $consulta->get_result();
+ 
     $total = $resultado->num_rows; 
     
     $div = '<div style="color:red; font-size:25px">'.$total.'</div>';
     // Construir el HTML para mostrar los clientes
     $html = '';
     while ($cliente = mysqli_fetch_assoc($resultado)) {   
+        
         $html .= '<tr>';
         $html .= '<td>'.$cliente['apellido'].'</td>';
         $html .= '<td>'.$cliente['nombre'].'</td>';
@@ -36,7 +38,7 @@ if(isset($_GET['cuota'])) {
         $html .= '<td>'.$cliente['num_cuotas'].'</td>';
         $html .= '<td>'.$cliente['fecha_emision'].'</td>';
         $html .= '<td>$'.number_format($cliente['costo'], 2).'</td>';
-        $html .= '<td class="primary"><a href="../edit/editPago.php?id='.$cliente['id'].'">Editar</a></td>';
+        $html .= '<td class="primary"><a href="/cliente/pagos/editar/'.$cliente['id_pagos'].'">Editar</a></td>';
         $html .= '</tr>';
     }
 
